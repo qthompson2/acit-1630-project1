6823f429925e5e6ef34caf43ae351e98ace82dba
q.thompson <q.thompson088@gmail.com>
2022-10-14 11:41:14 -0700

    Adds data constraints

--- warehouse.sql
+++ warehouse.sql
@@ -13,6 +13,7 @@ CREATE TABLE Facilities(
     Facility_Province Char(4),
     Facility_PostCode CHAR(6),
     Facility_Street CHAR(20),
+    Facility_Phone INT,
     PRIMARY KEY (Facility_ID)
 )
 
@@ -31,7 +32,7 @@ CREATE TABLE Employees(
     Facility_ID INT FOREIGN KEY REFERENCES Facilities(Facility_ID),
     Employee_LName CHAR(15),
     Employee_FName CHAR(15),
-    Employee_Salary MONEY,
+    Employee_Salary MONEY CHECK(Employee_Salary >= 0),
     Employee_Email CHAR(30),
     Employee_Phone INT,
     Employee_Address CHAR(30),
@@ -41,7 +42,7 @@ CREATE TABLE Employees(
 
 CREATE TABLE Lockers(
     Locker_ID INT,
-    Locker_Volume INT,
+    Locker_Volume INT NOT NULL,
     Customer_ID INT FOREIGN KEY REFERENCES Customers(Customer_ID),
     Facility_ID INT FOREIGN KEY REFERENCES Facilities(Facility_ID),
     PRIMARY KEY (Locker_ID, Facility_ID)
@@ -51,21 +52,21 @@ CREATE TABLE Lockers(
 CREATE TABLE Skills(
     Skill_ID INT,
     Skill_Name CHAR(20),
-    Skill_RenewPeriod INT,
+    Skill_RenewPeriod INT CHECK(Skill_RenewPeriod >= 0),
     PRIMARY KEY (Skill_ID)
 )
 
 CREATE TABLE Amenities(
     Amenity_ID INT,
     Amenity_Name CHAR(20),
-    Amenity_MaintPeriod INT,
+    Amenity_MaintPeriod INT CHECK(Amenity_MaintPeriod >= 0),
     PRIMARY KEY (Amenity_ID)
 )
 
 CREATE TABLE Jobs(
     Job_ID INT,
     Job_Name CHAR(20),
-    Job_Rate MONEY,
+    Job_Rate MONEY CHECK(Job_Rate >= 0),
     PRIMARY KEY (Job_ID)
 )
 
@@ -73,32 +74,29 @@ CREATE TABLE Jobs(
 CREATE TABLE Facility_Services(
     Facility_ID INT FOREIGN KEY REFERENCES Facilities(Facility_ID),
     Amenity_ID INT FOREIGN KEY REFERENCES Amenities(Amenity_ID),
-    Last_Maint DATE,
-    Next_Maint DATE, /*Derived*/
+    Last_Maintained DATE NOT NULL,
     PRIMARY KEY (Facility_ID, Amenity_ID)
 )
 
 CREATE TABLE Customer_Accounts(
     Customer_ID INT FOREIGN KEY REFERENCES Customers(Customer_ID),
     Facility_ID INT FOREIGN KEY REFERENCES Facilities(Facility_ID),
-    Account_Opened DATE,
-    Account_Length INT,
-    Account_End DATE, /*Derived*/
+    Account_Opened DATE NOT NULL,
+    Subscription_Length INT,
     PRIMARY KEY (Customer_ID, Facility_ID)
 )
 
 CREATE TABLE Certifications(
     Employee_ID INT FOREIGN KEY REFERENCES Employees(Employee_ID),
     Skill_ID INT FOREIGN KEY REFERENCES Skills(Skill_ID),
-    Certification_Date DATE,
-    Certification_Renew Date, /*Derived*/
+    Certification_Date DATE NOT NULL,
     PRIMARY KEY (Employee_ID, Skill_ID)
 )
 
 CREATE TABLE Assignments(
     Employee_ID INT FOREIGN KEY REFERENCES Employees(Employee_ID),
     Job_ID INT FOREIGN KEY REFERENCES Jobs(Job_ID),
-    Job_Start DATE,
+    Job_Start DATE NOT NULL,
     PRIMARY KEY (Employee_ID, Job_ID)
 )
 
